b0713744bb147e25a5d77ee49fae803b4160db66
Ernesto Urbino <ernesto.urbino@outlook.com>
2023-08-23 18:50:38 -0400

    Configure DbContext

--- SysMed.Data/SysmedContext.cs
+++ SysMed.Data/SysmedContext.cs
@@ -12,10 +12,29 @@ namespace SysMed.Data
         public DbSet<MedicalDevice> MedicalDevices { get; set; }
         public DbSet<Maintenance> Maintenances { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=SysMed;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<MedicalDevice>().ToTable("MedicalDevices");
             modelBuilder.Entity<Maintenance>().ToTable("Maintenances");
+
+            // Configuration using Fluent API
+            modelBuilder.Entity<MedicalDevice>()
+                .Property(p => p.Name)
+                .HasMaxLength(250)
+                .IsRequired();
+            modelBuilder.Entity<MedicalDevice>()
+                .Property(p => p.Brand)
+                .HasMaxLength(250)
+                .IsRequired();
+            modelBuilder.Entity<MedicalDevice>()
+                .Property(p => p.Model)
+                .HasMaxLength(250)
+                .IsRequired();
         }
     }
 }
