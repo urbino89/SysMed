b408035fbdf955a4cb182aaef74b55442bf2c070
Ernesto Urbino <ernesto.urbino@outlook.com>
2023-08-23 00:51:04 -0400

    Add initial proyects

---
+++ SysMed.Model/MedicalEquipment.cs
@@ -0,0 +1,15 @@
+﻿namespace SysMed.Model
+{
+    public class MedicalEquipment
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+    }
+}
